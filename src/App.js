fee1d04e04517645568d9a3bb63c6dd8b6f9d0a3
wsameer <dev.wsameer@gmail.com>
2021-10-09 16:51:05 +0530

    fix: adds missing Row component to fix the layout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.scss";
 import { Routes } from "./components/Routes";
 
-const App = () => {
-    return (
-        <div className="App">
-            <Router>
-                <Routes />
-            </Router>
-        </div>
-    );
-};
+const App = () => (
+    <div className="App">
+        <Router>
+            <Routes />
+        </Router>
+    </div>
+);
 
 export default App;
