7b4a18b4134ff2646cbd199e2440394f57a4f89e
error434 <65418777+error434@users.noreply.github.com>
2021-10-13 11:02:04 +0400

    add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./App.scss";
+import "./app.css";
 import { Routes } from "./components/Routes";
 
 const App = () => (
