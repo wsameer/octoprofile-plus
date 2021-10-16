7d9f69d0f87e65247a84dcfd3db100e650824aa9
wsameer <dev.wsameer@gmail.com>
2021-10-16 15:07:28 +0530

    refactor: refactors Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,50 +1,46 @@
-import React, { useState } from 'react';
-import { Col, Form } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { Col, Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
-const Home= () => {
-  const history = useHistory()
-  const [userName, setUserName] = useState('');
+const Home = () => {
+    const history = useHistory(),
+        [userName, setUserName] = useState(""),
+        handleChange = (event) => setUserName(event.target.value),
+        handleSubmit = (event) => {
+            event.preventDefault();
+            if (userName !== "") {
+                history.push({
+                    pathname: `/${userName}`,
+                    state: {
+                        id: userName,
+                    },
+                });
+            }
+        };
 
-  const handleChange = event => setUserName(event.target.value);
+    return (
+        <Col md={{ span: 4, offset: 4 }} className="start text-center mt-5">
+            <img
+                className="mb-4"
+                src="/github-plus.png"
+                alt="Logo"
+                height="120"
+            />
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formEmail">
+                    <Form.Label className="mb-4">
+                        Enter a GitHub user name
+                    </Form.Label>
+                    <Form.Control
+                        type="text"
+                        autoFocus
+                        value={userName}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+            </Form>
+        </Col>
+    );
+};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (userName !== '') {
-      history.push({
-        pathname: `/${userName}`,
-        state: {
-          id: userName
-        }
-      });
-    }
-  };
-
-  return (
-    <Col md={{ span: 4, offset: 4 }}
-      className="start text-center mt-5">
-      <img
-        className="mb-4"
-        src="/github-plus.png"
-        alt="Logo"
-        height="120"
-      />
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formEmail">
-          <Form.Label className="mb-4">
-            Enter a GitHub user name
-          </Form.Label>
-          <Form.Control
-            type="text"
-            autoFocus
-            value={userName}
-            onChange={handleChange}
-          />
-        </Form.Group>
-      </Form>
-    </Col>
-  )
-}
-
-export default Home
-
+export default Home;
