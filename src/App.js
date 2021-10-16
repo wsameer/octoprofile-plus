c6f6c9d92e8cd3568be001a7c0aa157f9d9830bf
error434 <65418777+error434@users.noreply.github.com>
2021-10-16 11:06:26 +0400

    Revert "add css"
    remove vanilla css
    
    This reverts commit 7b4a18b4134ff2646cbd199e2440394f57a4f89e.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./app.css";
+import "./App.scss";
 import { Routes } from "./components/Routes";
 
 const App = () => (
